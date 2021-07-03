638574d0e1058df2e365171a4e57664fbb61bc40
andre <andre@DESKTOP-PMTSH1N.lan>
2021-07-03 19:11:05 +0200

    fermate raggiungibili visita ampiezza profondita

--- src/main/java/it/polito/tdp/metroparis/model/Model.java
+++ src/main/java/it/polito/tdp/metroparis/model/Model.java
@@ -1,11 +1,15 @@
 package it.polito.tdp.metroparis.model;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 import org.jgrapht.Graph;
 import org.jgrapht.Graphs;
 import org.jgrapht.graph.DefaultEdge;
 import org.jgrapht.graph.SimpleGraph;
+import org.jgrapht.traverse.BreadthFirstIterator;
+import org.jgrapht.traverse.DepthFirstIterator;
 
 import it.polito.tdp.metroparis.db.MetroDAO;
 
@@ -49,6 +53,78 @@ public class Model {
 		System.out.format("Grafo creato con %d vertici e %d archi\n",
 				this.grafo.vertexSet().size(), this.grafo.edgeSet().size()) ;
 //		System.out.println(this.grafo) ;
+		
+		/*
+		// supponiamo di avere fermata e voler conoscere gli archi uscenti
+		
+		Fermata f;
+		
+		Set<DefaultEdge> archi = this.grafo.edgesOf(f);
+		
+		// data la fermata e ogni arco uscente vogliamo trovare verice opposto
+		
+		for(DefaultEdge e : archi) {
+			
+			// 1 METODO, archi non orientati.. non sappiamo chi sia origine e chi destinazione, necessari controlli
+			/*Fermata f1 = this.grafo.getEdgeSource(e);
+			Fermata f2 = this.grafo.getEdgeTarget(e);
+			
+			if(f.equals(f1)) {
+				// il vertice opposto è f2
+			} else {
+				// il vertice opposto è f1
+			}
+			*/
+			
+			// 2 METODO VELOCE ma sempre dentro ciclo
+			//Fermata f1 = Graphs.getOppositeVertex(this.grafo, e, f);
+		//}
+		
+		// 3 METODO PASSO DIRETTAMENTE A LISTA DI VERTICI ADIACENTI, successori o predecessori
+		//List<Fermata> fermateAdiacenti = Graphs.successorListOf(this.grafo, f);
+		
+	}
+	
+	// metodo per visita al grafo AMPIEZZA VS PROFONDITA
+	public List<Fermata> fermateRaggiungibili(Fermata partenza) {
+		
+		// AMPIEZZA
+		BreadthFirstIterator<Fermata, DefaultEdge> bfv = new BreadthFirstIterator<>(this.grafo, partenza);
+		
+		 List<Fermata> result = new ArrayList<>();
+		 
+		 while(bfv.hasNext()) {
+			 Fermata f = bfv.next();
+			 result.add(f);
+		 }
+		 
+		
+		/* PROFONDITA
+		 DepthFirstIterator<Fermata, DefaultEdge> dfv = new DepthFirstIterator<>(this.grafo, partenza);
+			
+		 List<Fermata> result = new ArrayList<>();
+		 
+		 while(dfv.hasNext()) {
+			 Fermata f = dfv.next();
+			 result.add(f);
+		 }
+		 */
+		 
+		 
+		 return result;
+	}
+	
+	
+	public Fermata trovaFermata(String nome) {
+		
+		for(Fermata f : this.grafo.vertexSet()) {
+			
+			if(f.getNome().equals(nome))
+				return f;
+			
+		}
+		
+		return null;
 	}
 	
 }
